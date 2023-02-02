d6baedd58f20cf5d49ddb3babf65847be01237e8
Anthony Panarello <aPanarello20@gmail.com>
2023-02-02 12:05:37 -0500

    Moved output to stdout by default; ports scanned by default are 1 - 1000

--- file_output.rs
+++ file_output.rs
@@ -1,39 +1,54 @@
-use std::fs::File;
-use std::io::prelude::*;
-use std::net::{TcpStream};
-use std::io::{ErrorKind};
-    
+use std::net::TcpStream;
+use std::io::ErrorKind;
+
+use std::env;
+
 fn main() {
     let host = "localhost";
-    let max_port = 65535;
+    let mut min_port = 0;
+    let mut max_port = 1000;
     let mut open_ports = 0;
     let mut closed_ports = 0;
-    let mut output = String::new();
-    output.push_str("Scanned 65535 ports\n");
+
+    let mut argv = env::args();
+    _ = argv.next();
+    while let Some(arg) = argv.next() {
+        match &arg[..] {
+            "-p" | "--ports" => {
+                min_port = argv.next().unwrap_or_else(|| {
+                    eprintln!("Missing min port");
+                    std::process::exit(1);
+                }).parse::<u16>().unwrap_or_else(|_| {
+                    eprintln!("Min port must be a number");
+                    std::process::exit(1);
+                });
+                max_port = argv.next().unwrap_or_else(|| {
+                    eprintln!("Missing max port");
+                    std::process::exit(1);
+                }).parse::<u16>().unwrap_or_else(|_| {
+                    eprintln!("Max port must be a number");
+                    std::process::exit(1);
+                });
+            },
+            _ => continue,
+        }
+    }
+
+    println!("Scanning {} ports", max_port - min_port);
 
     for port in 1..max_port {
         match TcpStream::connect((host, port)) {
             Ok(_) => {
                 open_ports += 1;
-                output.push_str(&format!("Port {} is open\n", port));
+                println!("Port {} is open", port);
             },
             Err(e) => match e.kind() {
-                ErrorKind::ConnectionRefused => {
-                    closed_ports += 1;
-                    output.push_str(&format!("Port {} is closed\n", port));
-                },
-                _ => {
-                println!("Error: {}", e);
-                output.push_str(&format!("Error: {}\n", e));
+                ErrorKind::ConnectionRefused => closed_ports += 1,
+                _ => println!("Error: {}", e),
             }
         }
     }
-}
 
-output.push_str(&format!("{} open ports\n", open_ports));
-output.push_str(&format!("{} closed ports\n", closed_ports));
-
-let mut file = File::create("port_scan_output.txt").unwrap();
-file.write_all(output.as_bytes()).unwrap();
-
+    println!("{} open ports", open_ports);
+    println!("{} closed ports", closed_ports);
 }
